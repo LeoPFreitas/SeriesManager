015e9555d1476850618706aa9a73ca16b2a45208
Leonardo Freitas <lfreitas.lpf@gmail.com>
2021-10-25 20:31:36 -0300

    Inflate layout

--- app/src/main/java/com/example/projetoseriesmanager/MainActivity.kt
+++ app/src/main/java/com/example/projetoseriesmanager/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.projetoseriesmanager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projetoseriesmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val activityMainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    // Data source
+//    private val
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activityMainBinding.root)
     }
 }
