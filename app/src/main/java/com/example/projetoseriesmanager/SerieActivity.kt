28b61bc81ebc6953610db84fbc01bd37eea71c09
Leonardo Freitas <lfreitas.lpf@gmail.com>
2021-10-31 14:34:07 -0300

    Add SeriesActivity and layouts

---
+++ app/src/main/java/com/example/projetoseriesmanager/SerieActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projetoseriesmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SerieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_serie)
+    }
+}
